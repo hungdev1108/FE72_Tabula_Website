0902ba7d543b37dbbda428a9a11da191ef60305e
hungdev1108 <user.name>
2022-06-03 16:24:07 +0700

    fix header fixed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 new WOW().init();
+
+// Scroll fixed header
+$(function () {
+  var fixedHeader = 500;
+  $(window).scroll(function () {
+    var scroll = getCurrentScroll();
+    if (scroll > fixedHeader) {
+      $(".collapse").addClass("header-fixed");
+      $(".navbar-nav").addClass("header-nav-fixed");
+    } else {
+      $(".collapse").removeClass("header-fixed");
+      $(".navbar-nav").removeClass("header-nav-fixed");
+    }
+  });
+});
+
+function getCurrentScroll() {
+  return window.pageYOffset;
+}
